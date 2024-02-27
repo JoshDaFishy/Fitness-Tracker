77f7320266632d085610dacb430cf3a72267e17a
JoshDaFishy <computergeek328@gmail.com>
2024-02-27 14:50:59 +0000

    Front end is now functioning for page one. I will now work on getting the database working

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import sqlite3
-import PySimpleGUI
+import PySimpleGUI as sg
 
-layout = [ [PySimpleGUI.Text('Hello, world!')] ]
-window = PySimpleGUI.Window('Hello Example', layout)
+sg.theme('DarkAmber')
+
+names = ['test1', 'test2', 'test3', 'test4', 'test5']
+
+l2=sg.Text("test on page 2")
+tab1= [     [sg.Text('Exercise'), sg.Combo(names, key="inp", enable_events=True, default_value='')],
+            [sg.Text('Total Weight'), sg.InputText(do_not_clear=False)],]
+tab2=[[l2]]
+
+layout = [
+            [sg.TabGroup([
+            [sg.Tab('Data Entry', tab1),
+            sg.Tab('Review Data', tab2)]])],
+            #[psg.OK(), psg.Cancel()]
+            [sg.Button('Submit'), sg.Button('Clear')] ]
+
+# tab2=sg.Tab("title2", l2)
+# Tg = sg.TabGroup([[tab1, tab2]])
+
+window = sg.Window('Hello Example', layout)
 while True:
     event, values = window.read()
-    if event == PySimpleGUI.WIN_CLOSED:
+    if event == sg.WIN_CLOSED:
         break
+    elif event == 'inp':
+        print(values["inp"])
+    elif event == "Submit":
+        print("submiting data")
+        window['inp'].update('')
+    elif event == "Clear":
+        print("clearing page")
+        window['inp'].update('')
 window.close()
